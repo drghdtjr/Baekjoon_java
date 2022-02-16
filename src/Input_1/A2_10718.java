8cd5cd4d8d9033bce149788d7c248193e7f74c5b
drghdtjr <drghdtjr@naver.com>
2022-02-16 23:01:39 +0900

    2022.02.16

--- src/Input_1/A2_10718.java
+++ src/Input_1/A2_10718.java
@@ -1,4 +1,8 @@
 package Input_1;
 
 public class A2_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
 }
--- src/Input_1/A3_10171.java
+++ src/Input_1/A3_10171.java
@@ -1,4 +1,8 @@
 package Input_1;
 
 public class A3_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println("\\    /\\");
+    }
 }
