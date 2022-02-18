906f6b96cbf3b371144f007149758bea327dd787
drghdtjr <drghdtjr@naver.com>
2022-02-18 13:07:02 +0900

    2022.02.18

--- src/If_2/B5_2884.java
+++ src/If_2/B5_2884.java
@@ -1,5 +1,5 @@
 
-/*
+
 package If_2;
 
 
@@ -7,24 +7,20 @@ import java.util.Scanner;
 
 public class B5_2884 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int h= sc.nextInt();
-        int m= sc.nextInt();
-        if(h>0) {
-            if (m >= 45) {
-                System.out.println(h + " " + (m - 45));
-            } else {
-                System.out.println((h - 1) + " " + (45 - m));
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        if(m<45)
+        {
+            h--;
+            m=60-(45-m);
+            if(h<0){
+                h=23;
             }
+            System.out.println(h+" "+m);
         }
         else{
-            if (m >= 45) {
-                System.out.println(h + " " + (m - 45));
-            } else {
-                System.out.println("23" + " " + (45 - m));
-            }
-
+            System.out.println(h+" "+(m-45));
         }
     }
 }
-*/
--- src/If_2/B6_2525.java
+++ src/If_2/B6_2525.java
@@ -1,4 +1,19 @@
 package If_2;
 
+import java.util.Scanner;
+
 public class B6_2525 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int hour= sc.nextInt();
+        int minute= sc.nextInt();
+        int cook= sc.nextInt();
+        int cook_hour=cook/60;
+        int cook_minute=cook%60;
+        if(minute+cook>60);
+            hour++;
+            minute=
+        }
+
+    }
 }
