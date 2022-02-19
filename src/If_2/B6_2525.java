152bbfa3fe1e23ea68e72fe17c01ef2677096865
drghdtjr <drghdtjr@naver.com>
2022-02-19 23:15:36 +0900

    2022.02.19

--- src/If_2/B6_2525.java
+++ src/If_2/B6_2525.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 
 public class B6_2525 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int hour= sc.nextInt();
-        int minute= sc.nextInt();
-        int cook= sc.nextInt();
-        int cook_hour=cook/60;
-        int cook_minute=cook%60;
-        if(minute+cook>60);
-            hour++;
-            minute=
-        }
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int value = sc.nextInt();
 
+        minute += value;
+        if (minute >= 60) {
+            while (minute >= 60) {
+                minute -= 60;
+                hour++;
+                if (hour >= 24) {
+                    hour = 0;
+                }
+
+            }
+
+        }
+        System.out.println(hour+" "+minute);
     }
 }
