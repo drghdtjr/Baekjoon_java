f6376b1d5234f2a8c10c67142d64846050b66726
drghdtjr <drghdtjr@naver.com>
2022-02-23 23:00:13 +0900

    2022.02.23

--- src/for_3/C4_15552.java
+++ src/for_3/C4_15552.java
@@ -1,16 +1,26 @@
 package for_3;
 
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class C4_15552 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int t= sc.nextInt();
-        for(int i=0;i<t;i++)
-        {
-            int a= sc.nextInt();
-            int b= sc.nextInt();
-            System.out.println(a+b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for(int i=0; i < n; i++){
+            st=new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            int sum=a+b;
+            bw.write(sum+"\n");
         }
+        bw.flush();
+
     }
 }
+
