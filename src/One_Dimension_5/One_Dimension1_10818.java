5a3f1224667cece807ec635140916a9cc6176600
drghdtjr <drghdtjr@naver.com>
2022-02-25 23:34:38 +0900

    2022.02.25

--- src/One_Dimension_5/One_Dimension1_10818.java
+++ src/One_Dimension_5/One_Dimension1_10818.java
@@ -1,4 +1,18 @@
 package One_Dimension_5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class One_Dimension1_10818 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int[]num=new int[n];
+        for(int i=0; i<n;i++){
+            num[i]= sc.nextInt();
+        }
+        Arrays.sort(num);
+
+        System.out.print((num[0])+" "+num[num.length-1]);
+    }
 }
