e99a17d68ee5ec68ae94a676d494f22fb86a8a6d
drghdtjr <drghdtjr@naver.com>
2022-03-02 15:51:24 +0900

    2022.03.02

--- src/One_Dimension_5/One_Dimension6_8958.java
+++ src/One_Dimension_5/One_Dimension6_8958.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class One_Dimension6_8958 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        String str;
+        for(int i=0; i<num;i++){
+            str=sc.next();
+            int sum=0, cnt=0;
+            for(int j=0; j<str.length();j++){
+                if(str.charAt(j)=='O'){
+                    cnt++;
+                    sum+=cnt;
 
+                }
+                else cnt=0;
+            }
+            System.out.println(sum);
+        }
     }
 }
--- src/One_Dimension_5/One_Dimension7_4344.java
+++ src/One_Dimension_5/One_Dimension7_4344.java
@@ -1,4 +1,12 @@
 package One_Dimension_5;
 
+import java.util.Scanner;
+
 public class One_Dimension7_4344 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int c= sc.nextInt();
+        int n= sc.nextInt();
+
+    }
 }
