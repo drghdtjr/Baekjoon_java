355d8ca128d9f2258b31a95cf30d501e2d3e184f
drghdtjr <drghdtjr@naver.com>
2022-03-03 22:44:50 +0900

    2022.03.03

--- src/One_Dimension_5/One_Dimension7_4344.java
+++ src/One_Dimension_5/One_Dimension7_4344.java
@@ -6,7 +6,25 @@ public class One_Dimension7_4344 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int c= sc.nextInt();
-        int n= sc.nextInt();
+        int []arr;
+        for(int i=0; i<c;i++){
+            int n= sc.nextInt();
+            arr=new int[n];
+            int sum=0;
+            for(int j=0;j<n;j++){
+                int point=sc.nextInt();
+                arr[j]=point;
+                sum+=point;
+            }
+            double avg=(double) sum/n;
+            double cnt=0;
+            for(int j=0; j<n;j++){
+                if(arr[j]>avg){
+                    cnt++;
+                }
+            }
+            System.out.printf("%.3f%%\n",cnt / n * 100);
+        }
 
     }
 }
