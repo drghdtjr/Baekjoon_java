fd98ef896972241e2f70bce6fd323a315c443e19
drghdtjr <drghdtjr@naver.com>
2022-03-04 18:37:36 +0900

    2022.03.04

--- src/function_6/function1_15596.java
+++ src/function_6/function1_15596.java
@@ -3,14 +3,16 @@ package function_6;
 import java.util.Scanner;
 
 public class function1_15596 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        int sum=Test(n);
-        System.out.println(sum);
-    }
-    public static int Test(int a)
-    {
-        return (a*(a+1))/2;
+    long sum(int[] a) {
+        long ans = 0;
+        for (int i = 0; i < a.length; i++) {
+            ans += a[i];
+        }
+        return ans;
     }
 }
+
+
+
+
+
--- src/function_6/function2_4673.java
+++ src/function_6/function2_4673.java
@@ -1,4 +1,12 @@
 package function_6;
 
 public class function2_4673 {
+    public static void main(String[] args) {
+
+    }
+    public static int d(int n){
+
+
+
+    }
 }
