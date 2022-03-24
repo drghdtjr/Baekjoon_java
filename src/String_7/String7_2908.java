784658e0290f9aeee7550e52fb30a1e4b2762afa
drghdtjr <drghdtjr@naver.com>
2022-03-24 15:13:36 +0900

    2022.03.24

--- src/String_7/String7_2908.java
+++ src/String_7/String7_2908.java
@@ -1,4 +1,20 @@
 package String_7;
 
+import java.util.Scanner;
+
 public class String7_2908 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a= reverse_num(sc.nextInt());
+        int b=reverse_num(sc.nextInt());
+        System.out.println(a>b?a:b);
+
+
+
+    }
+    public static int reverse_num(int a){
+        a = ((a % 10) * 100) + (((a % 100) / 10) * 10) + (a / 100);
+        return a;
+    }
+
 }
