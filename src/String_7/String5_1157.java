bc1c0b44d06c689bdfb5fe6657524bcf5e0810e5
drghdtjr <drghdtjr@naver.com>
2022-03-25 22:59:39 +0900

    2022.03.25

--- src/String_7/String5_1157.java
+++ src/String_7/String5_1157.java
@@ -8,8 +8,32 @@ public class String5_1157 {
         String word= sc.next();
         int []arr=new int [26];
         for(int i=0; i<word.length();i++){
+            if('A'<=word.charAt(i)&&word.charAt(i)<='Z'){
+                arr[word.charAt(i)-'A']++;
 
+            }
+            else{
+                arr[word.charAt(i)-'a']++;
+
+            }
         }
+        int max=-1;
+        char ch='?';
+
+        for(int i=0; i<26;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                ch=(char)(i+65);
+
+            }
+            else if (arr[i]==max){
+                ch='?';
+
+            }
+        }
+
+        System.out.println(ch);
+
 
     }
 }
