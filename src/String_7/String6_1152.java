275a754680bcfa0c35e635133e99544f49af8d4f
drghdtjr <drghdtjr@naver.com>
2022-03-31 16:08:16 +0900

    2022.03.31

--- src/String_7/String6_1152.java
+++ src/String_7/String6_1152.java
@@ -1,4 +1,18 @@
 package String_7;
 
+import java.util.Scanner;
+
 public class String6_1152 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String words= sc.nextLine().trim();
+        if(words.isEmpty()){
+            System.out.println("0");
+
+        }
+        else{
+            System.out.println(words.split(" ").length);
+        }
+    }
 }
