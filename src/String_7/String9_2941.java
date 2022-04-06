496da2bfe8f9fd0b1e996563549ac5530ea3bf3b
drghdtjr <drghdtjr@naver.com>
2022-04-06 19:35:55 +0900

    2022.04.06

--- src/String_7/String9_2941.java
+++ src/String_7/String9_2941.java
@@ -1,4 +1,18 @@
 package String_7;
 
+import java.util.Scanner;
+
 public class String9_2941 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String str[]={"c=","c-","dz=","d-","lj","nj","s=","z="};
+
+        for(int i=0; i<str.length;i++){
+            if(s.contains(str[i])){
+                s=s.replace(str[i],"0");
+            }
+        }
+        System.out.println(s.length());
+    }
 }
