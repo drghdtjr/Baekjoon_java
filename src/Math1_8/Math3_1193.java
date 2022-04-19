e5ce428f83031534b337858cdec2d1c774bdcd17
drghdtjr <drghdtjr@naver.com>
2022-04-19 17:20:23 +0900

    2022.04.14

--- src/Math1_8/Math3_1193.java
+++ src/Math1_8/Math3_1193.java
@@ -1,4 +1,11 @@
 package Math1_8;
 
+import java.util.Scanner;
+
 public class Math3_1193 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+
+    }
 }
